1097cd3b01e0cb69cfc63c9e4cf49d060eec1acc
Lucy Suddenly <suddenlylucy@gmail.com>
2021-05-07 08:55:06 -0700

    fix: controller wiring and environment

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -1,6 +1,8 @@
-class Api::V1::ServicesController < ApplicationController
+class Api::V1::ServicesController < Api::V1::ApplicationController
     include PdClient
 
     def show 
+        query = params[:id]
+        # call api client
     end
 end
