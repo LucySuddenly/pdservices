f6fcc1dcd5856fabf94a15e309847052e650f65c
Lucy Suddenly <suddenlylucy@gmail.com>
2021-05-09 10:03:18 -0700

    refactor: little nits

--- lib/pd_client.rb
+++ lib/pd_client.rb
@@ -6,10 +6,10 @@ module PdClient
 
         private
 
-        mattr_accessor :api_key
-        @@PD_TOKEN = Rails.configuration.pd_token
+        # make configurable per-request to enable service for multiple PD tokens/accounts/customers
+        @@PD_TOKEN ||= Rails.configuration.pd_token
 
-        # production apps need configureable per-env resources
+        # production apps need configurable per-env resources
         self.base_uri 'https://api.pagerduty.com'
         self.headers 'Authorization': "Token token=#{@@PD_TOKEN}"
         self.format :json
@@ -19,7 +19,7 @@ module PdClient
         raise ArgumentError, 'query cannot be empty' if query == ""
 
         resp = self.get_with_retries '/services', { query: "query=" + query }
-        resp = resp.with_indifferent_access
+
         resp[:services].sort_by {|s| s[:name] }.first
     end
 
@@ -30,7 +30,7 @@ module PdClient
             attempts += 1
             # add more rubust resp code handling and configurable timeout duration
 
-            WebClient.get(path, options.merge(timeout: 2)).parsed_response
+            WebClient.get(path, options.merge(timeout: 2)).parsed_response.with_indifferent_access
         rescue HTTParty::Error, StandardError => e
             # make configurable
             if attempts < 3 
--- test/controllers/api/v1/services_controller_test.rb
+++ test/controllers/api/v1/services_controller_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class Api::V1::ServicesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+  # test "#show" do
+  #   PdClient.stubs(:get_first_matching_service)
+  #   what = get api_v1_service_path('v')
+
   # end
 end
